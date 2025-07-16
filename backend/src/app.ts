8861cb1fd698e0975bbe5b8e0e445ecd26866ab8
ashikreserveit17 <ashik.bhuyan@reserveitbd.com>
2025-07-16 12:24:58 +0600

    feat: logger set

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,8 @@ import express, { Application, NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
 import router from './app/routes'
+import { errorlogger } from './shared/logger'
+import { LogsRoutes } from './app/modules/Logs/logs.routes'
 
 const app: Application = express()
 
@@ -15,19 +17,56 @@ app.use(express.urlencoded({ extended: true }))
 // Application routes
 console.log(app.get('env'))
 app.use('/api/v1', router)
+app.use('/logs', LogsRoutes)
 // app.use('/api/v1/users/', UserRoutes)
 // app.use('/api/v1/academic-semesters/', AcademicSemesterRoutes)
 
 //Testing
-app.get('/', async (req: Request, res: Response) => {
-  res.send('Working Successfully')
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).send(`
+   <html>
+      <head>
+        <title>Docker Logs Viewer</title>
+        <link rel="stylesheet" href="/styles.css">
+      </head>
+      <body>
+        <h1>Welcome to the Docker Logs Viewer Page!</h1>
+        <p>Go to <a href="/logs/errors">Error Logs</a> or <a href="/logs/successes">Success Logs</a>.</p>
+      </body>
+    </html>
+  `)
 })
-// app.get('/', (req: Request, res: Response) => {
-//   throw new ApiError(400, 'ore baba error')
-//   //next('next function er bitore kichu likle eta error bujay ja global error handle kore') //Error
-// })
+app.get('/todos', async (req: Request, res: Response) => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+  // const response = await fetch('http://ts-docker-container:5000/api/v1/users')
+  const todos = await response.json()
+  return res.status(200).json(todos)
+})
 //global error handler
-app.use(globalErrorHandler)
+// app.use(globalErrorHandler)
+
+app.get('/error', (req: Request, res: Response) => {
+  throw new Error('This is a forced error!')
+})
+// Error handler
+app.use((err: Error, req: Request, res: Response, next: any) => {
+  console.error(err)
+  errorlogger.error(err)
+
+  res.status(500).send(`
+    <html>
+      <head>
+        <title>Error</title>
+        <link rel="stylesheet" href="/styles.css">
+      </head>
+      <body>
+        <h1>Something went wrong</h1>
+        <p>${err.message}</p>
+        <a href="/">Back to Home</a>
+      </body>
+    </html>
+  `)
+})
 
 //handle not found
 app.use((req: Request, res: Response, next: NextFunction) => {
